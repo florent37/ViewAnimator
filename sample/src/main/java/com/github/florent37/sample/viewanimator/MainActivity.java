05af15e23eba4efe56268c6f379e1f4ec633e542
gzu-liyujiang <liyujiang_tk@yeah.net>
2016-01-25 12:38:00 +0800

    add some enhanced animations, such as slideLeft,slit,fall,.. add path animation, use path of android or svg; add repeat count and repeat mode; update screenshots;

--- sample/src/main/java/com/github/florent37/sample/viewanimator/MainActivity.java
+++ sample/src/main/java/com/github/florent37/sample/viewanimator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.github.florent37.sample.viewanimator;
 
 import android.graphics.Color;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 
 import com.github.florent37.viewanimator.AnimationListener;
 import com.github.florent37.viewanimator.ViewAnimator;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,10 +54,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final View btnShake = findViewById(R.id.shake);
-        btnShake.setOnClickListener(new View.OnClickListener() {
+        final View btnPath = findViewById(R.id.path);
+        btnPath.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                ViewAnimator.animate(btnShake).shake().duration(3000).start();
+                final int[] START_POINT = new int[]{ 300, 270 };
+                final int[] BOTTOM_POINT = new int[]{ 300, 400 };
+                final int[] LEFT_CONTROL_POINT = new int[]{ 450, 200 };
+                final int[] RIGHT_CONTROL_POINT = new int[]{ 150, 200 };
+                Path path = new Path();
+                path.moveTo(START_POINT[0], START_POINT[1]);
+                path.quadTo(RIGHT_CONTROL_POINT[0], RIGHT_CONTROL_POINT[1], BOTTOM_POINT[0], BOTTOM_POINT[1]);
+                path.quadTo(LEFT_CONTROL_POINT[0], LEFT_CONTROL_POINT[1], START_POINT[0], START_POINT[1]);
+                path.close();
+                ViewAnimator.animate(btnPath).path(path).repeatCount(-1).start();
+            }
+        });
+
+        final View btnSvgPath = findViewById(R.id.svg_path);
+        btnSvgPath.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                final String SVG_PATH = "M 42.266949,70.444915 C 87.351695,30.995763 104.25847,28.177966 104.25847,28.177966 l 87.3517,36.631356 8.45339,14.088983 L 166.25,104.25847 50.720339,140.88983 c 0,0 -45.0847458,180.33898 -39.449153,194.42797 5.635594,14.08898 67.627119,183.15678 67.627119,183.15678 l 16.90678,81.7161 c 0,0 98.622885,19.72457 115.529665,22.54237 16.90678,2.8178 70.44491,-22.54237 78.8983,-33.81356 8.45339,-11.27118 76.08051,-107.07627 33.81356,-126.80085 -42.26695,-19.72457 -132.43644,-56.35593 -132.43644,-56.35593 0,0 -33.81356,-73.26271 -19.72458,-73.26271 14.08899,0 132.43644,73.26271 138.07204,33.81356 5.63559,-39.44915 19.72457,-169.0678 19.72457,-169.0678 0,0 28.17797,-25.36017 -28.17796,-19.72457 -56.35593,5.63559 -95.80509,11.27118 -95.80509,11.27118 l 42.26695,-87.35169 8.45339,-28.177968";
+                ViewAnimator.animate(btnSvgPath).svgPath(SVG_PATH).repeatCount(1).start();
             }
         });
     }
@@ -98,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
                 .thenAnimate(percent)
                     .custom(new AnimationListener.Update<TextView>() {
                         @Override public void update(TextView view, float value) {
-                            view.setText(String.format("%.02f%%", value));
+                            view.setText(String.format(Locale.US, "%.02f%%", value));
                         }
                     }, 0, 1)
 
--- viewanimator/src/main/java/com/github/florent37/viewanimator/ViewAnimator.java
+++ viewanimator/src/main/java/com/github/florent37/viewanimator/ViewAnimator.java
@@ -1,47 +1,65 @@
 package com.github.florent37.viewanimator;
 
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ValueAnimator;
+import android.support.annotation.IntDef;
+import android.support.annotation.IntRange;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.animation.Interpolator;
 
-import com.nineoldandroids.animation.Animator;
-import com.nineoldandroids.animation.AnimatorSet;
-
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by florentchampigny on 22/12/2015.
+ * Modified by gzu-liyujiang on 24/01/2016.
  */
 public class ViewAnimator {
+    private static final long DEFAULT_DURATION = 3000;
 
-    List<AnimationBuilder> animationList = new ArrayList<>();
-    Long duration = null;
-    Long startDelay = null;
-    Interpolator interpolator = null;
+    private List<AnimationBuilder> animationList = new ArrayList<>();
+    private long duration = DEFAULT_DURATION;
+    private long startDelay = 0;
+    private Interpolator interpolator = null;
 
-    AnimatorSet animatorSet;
-    View waitForThisViewHeight = null;
+    private int repeatCount = 0;
+    private int repeatMode = ValueAnimator.RESTART;
 
-    AnimationListener.Start startListener;
-    AnimationListener.Stop stopListener;
+    private AnimatorSet animatorSet;
+    private View waitForThisViewHeight = null;
 
-    ViewAnimator prev = null;
-    ViewAnimator next = null;
+    private AnimationListener.Start startListener;
+    private AnimationListener.Stop stopListener;
 
-    public static AnimationBuilder animate(View...view) {
+    private ViewAnimator prev = null;
+    private ViewAnimator next = null;
+
+    /**
+     * The interface Repeat mode.
+     */
+    @IntDef(flag = false, value = {ValueAnimator.RESTART, ValueAnimator.REVERSE})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface RepeatMode {
+        //代替enum，据说枚举类极其耗费内存
+    }
+
+    public static AnimationBuilder animate(View... view) {
         ViewAnimator viewAnimator = new ViewAnimator();
         return viewAnimator.addAnimationBuilder(view);
     }
 
-    public AnimationBuilder thenAnimate(View...views) {
+    public AnimationBuilder thenAnimate(View... views) {
         ViewAnimator nextViewAnimator = new ViewAnimator();
         this.next = nextViewAnimator;
         nextViewAnimator.prev = this;
         return nextViewAnimator.addAnimationBuilder(views);
     }
 
-    public AnimationBuilder addAnimationBuilder(View...views) {
+    public AnimationBuilder addAnimationBuilder(View... views) {
         AnimationBuilder animationBuilder = new AnimationBuilder(this, views);
         animationList.add(animationBuilder);
         return animationBuilder;
@@ -60,22 +78,30 @@ public class ViewAnimator {
             }
         }
 
+        for (Animator animator : animators) {
+            if (animator instanceof ValueAnimator) {
+                ValueAnimator valueAnimator = (ValueAnimator) animator;
+                valueAnimator.setRepeatCount(repeatCount);
+                valueAnimator.setRepeatMode(repeatMode);
+            }
+        }
+
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(animators);
 
-        if (duration != null)
-            animatorSet.setDuration(duration);
-        if (startDelay != null)
-            animatorSet.setDuration(startDelay);
+        animatorSet.setDuration(duration);
+        animatorSet.setStartDelay(startDelay);
         if (interpolator != null)
             animatorSet.setInterpolator(interpolator);
 
         animatorSet.addListener(new Animator.AnimatorListener() {
-            @Override public void onAnimationStart(Animator animation) {
+            @Override
+            public void onAnimationStart(Animator animation) {
                 if (startListener != null) startListener.onStart();
             }
 
-            @Override public void onAnimationEnd(Animator animation) {
+            @Override
+            public void onAnimationEnd(Animator animation) {
                 if (stopListener != null) stopListener.onStop();
                 if (next != null) {
                     next.prev = null;
@@ -83,11 +109,13 @@ public class ViewAnimator {
                 }
             }
 
-            @Override public void onAnimationCancel(Animator animation) {
+            @Override
+            public void onAnimationCancel(Animator animation) {
 
             }
 
-            @Override public void onAnimationRepeat(Animator animation) {
+            @Override
+            public void onAnimationRepeat(Animator animation) {
 
             }
         });
@@ -96,21 +124,23 @@ public class ViewAnimator {
     }
 
     public ViewAnimator start() {
-        if (prev != null)
+        if (prev != null) {
             prev.start();
-        else {
+        } else {
             animatorSet = createAnimatorSet();
 
-            if (waitForThisViewHeight != null)
+            if (waitForThisViewHeight != null) {
                 waitForThisViewHeight.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-                    @Override public boolean onPreDraw() {
+                    @Override
+                    public boolean onPreDraw() {
                         animatorSet.start();
                         waitForThisViewHeight.getViewTreeObserver().removeOnPreDrawListener(this);
                         return false;
                     }
                 });
-            else
+            } else {
                 animatorSet.start();
+            }
         }
         return this;
     }
@@ -135,7 +165,27 @@ public class ViewAnimator {
         return this;
     }
 
-    //region callbacks
+    /**
+     * Repeat count of animation.
+     *
+     * @param repeatCount the repeat count
+     * @return the view animation
+     */
+    public ViewAnimator repeatCount(@IntRange(from = -1) int repeatCount) {
+        this.repeatCount = repeatCount;
+        return this;
+    }
+
+    /**
+     * Repeat mode view animation.
+     *
+     * @param repeatMode the repeat mode
+     * @return the view animation
+     */
+    public ViewAnimator repeatMode(@RepeatMode int repeatMode) {
+        this.repeatMode = repeatMode;
+        return this;
+    }
 
     public ViewAnimator onStart(AnimationListener.Start startListener) {
         this.startListener = startListener;
@@ -147,15 +197,16 @@ public class ViewAnimator {
         return this;
     }
 
-    //endregion
-
-    //region interpolator
-
+    /**
+     * Interpolator view animator.
+     *
+     * @param interpolator the interpolator
+     * @return the view animator
+     * @link https://github.com/cimi-chen/EaseInterpolator
+     */
     public ViewAnimator interpolator(Interpolator interpolator) {
         this.interpolator = interpolator;
         return this;
     }
-
-    //endregion
 
 }
